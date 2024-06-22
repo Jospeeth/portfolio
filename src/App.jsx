8c85d056f1cfb7e9b8bc242cb63a09be6f24799f
jose Pertuz <japertuzd@gmail.com>
2024-06-21 22:21:47 -0500

    chore: Add framer-motion library for animations and update font-family in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import "./App.css";
 import HeroSection from "./components/HeroSection";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar"; 
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectSection";
 
 function App() {
 
   return (
-    <main className="flex min-h-screen flex-col ">
+    <main className="flex min-h-screen flex-col bg-background">
       <NavBar/>
-      <div className="container mt-24 mx-auto px-12 py-4  ">
+      <div className="container mt-24 px-12 py-4  ">
         <HeroSection />
+        <AboutSection />
+        <ProjectsSection />
       </div>
     </main>
   );
